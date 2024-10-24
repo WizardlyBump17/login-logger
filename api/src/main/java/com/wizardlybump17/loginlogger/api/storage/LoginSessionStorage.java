427fead84dccbaad865c1e5968b1aeb4ca17263b
WizardlyBump17 <davicraft130@gmail.com>
2024-10-24 00:29:38 -0300

    fixed the spaces

--- api/src/main/java/com/wizardlybump17/loginlogger/api/storage/LoginSessionStorage.java
+++ api/src/main/java/com/wizardlybump17/loginlogger/api/storage/LoginSessionStorage.java
@@ -9,23 +9,23 @@ import java.util.List;
 
 public interface LoginSessionStorage {
 
-    @NotNull LoginSession store(@NotNull LoginSession session)throws LoginSessionStorageException;
+    @NotNull LoginSession store(@NotNull LoginSession session) throws LoginSessionStorageException;
 
-    @Nullable LoginSession getById(int id)throws LoginSessionStorageException;
+    @Nullable LoginSession getById(int id) throws LoginSessionStorageException;
 
-    @NotNull List<LoginSession> getAll()throws LoginSessionStorageException;
+    @NotNull List<LoginSession> getAll() throws LoginSessionStorageException;
 
-    @NotNull LoginSession update(@NotNull LoginSession session)throws LoginSessionStorageException;
+    @NotNull LoginSession update(@NotNull LoginSession session) throws LoginSessionStorageException;
 
-    boolean delete(int id)throws LoginSessionStorageException;
+    boolean delete(int id) throws LoginSessionStorageException;
 
-    default boolean delete(@NotNull LoginSession session)throws LoginSessionStorageException {
+    default boolean delete(@NotNull LoginSession session) throws LoginSessionStorageException {
         return delete(session.getId());
     }
 
-    boolean isStored(int id)throws LoginSessionStorageException;
+    boolean isStored(int id) throws LoginSessionStorageException;
 
-    default boolean isStored(@NotNull LoginSession session)throws LoginSessionStorageException {
+    default boolean isStored(@NotNull LoginSession session) throws LoginSessionStorageException {
         return isStored(session.getId());
     }
 }
